2212dcf903c855e35e0300803619cdc7e9a6daee
kchristin22 <christinakoutsou22@gmail.com>
2023-10-07 15:24:19 +0300

    Make blelloch scan inclusive

--- blelloch.cpp
+++ blelloch.cpp
@@ -12,10 +12,18 @@ void fnSum(uint16_t* array, size_t start, size_t end){
     array[end] += array[start];
 }
 
-void fnSwap(uint16_t* array, size_t left, size_t right){
+void fnSweep(uint16_t* array, size_t left, size_t right){
     uint16_t temp = array[left];
     array[left] = array[right];
     array[right] += temp;
+}
+
+void fnShift(uint16_t* array, uint16_t* auxArray, size_t left, size_t right){
+    auxArray[left] = array[right];
+}
+
+void fnEquate(uint16_t* array, uint16_t* auxArray, size_t pos){
+    array[pos] = auxArray[pos];
 }
 
 
@@ -78,7 +86,7 @@ void downSweep(uint16_t* array, size_t size){
 
         for (size_t chunk = 0; chunk < chunks; chunk++){
             for (size_t i= ((1 << k) -1 + chunk*chunkSize); i <= (size - (1 << k) - (chunkSize*(chunks-chunk-1))); i+= (1 << (k+1))){ // subtracting (chunkSize*(chunks-chunk-1)) to account for the chunk's limit
-                threadObj.emplace_back(std::thread(fnSwap, array, i, (i + (1 << k))));
+                threadObj.emplace_back(std::thread(fnSweep, array, i, (i + (1 << k))));
             }
 
             for (std::thread& t : threadObj) {
@@ -90,10 +98,85 @@ void downSweep(uint16_t* array, size_t size){
     }
 }
 
+void leftShift(uint16_t* array, size_t size, uint16_t lastItem){
+    uint8_t nthreads = std::thread::hardware_concurrency();
+    uint8_t threadsToCreate = size / 2;
+    std::vector<std::thread> threadObj;
+    threadObj.reserve(nthreads);
+    size_t chunks = 1, chunkSize = size;
+
+    uint16_t* auxArray = (uint16_t*)malloc(SIZE*sizeof(uint16_t));
+
+    if(threadsToCreate >= nthreads){
+        if (threadsToCreate % nthreads == 0){
+            chunks = threadsToCreate / nthreads;
+        }
+        else{
+            chunks = threadsToCreate / nthreads + 1;
+        }
+        chunkSize = size/chunks;
+    }
+
+    for (size_t chunk = 0; chunk < chunks; chunk++){
+        for (size_t i = chunk*chunkSize; i<(size - 1 - (chunkSize * (chunks - chunk - 1))); i+=2){
+            threadObj.emplace_back(std::thread(fnShift, array, auxArray, i, i + 1));    // even items
+        }
+
+        for (std::thread& t : threadObj) {
+                if (t.joinable()) {
+                    t.join();
+                }
+        }
+
+        for (size_t i = 1 + chunk*chunkSize; i<(size-1); i+=2){
+            threadObj.emplace_back(std::thread(fnShift, array, auxArray, i, i + 1));    // odd items
+        }
+
+        for (std::thread& t : threadObj) {
+                if (t.joinable()) {
+                    t.join();
+                }
+        }
+    }
+
+    threadsToCreate = size;
+    chunks = 1;
+    chunkSize = size;
+
+    if(threadsToCreate >= nthreads){
+        if (threadsToCreate % nthreads == 0){
+            chunks = threadsToCreate / nthreads;
+        }
+        else{
+            chunks = threadsToCreate / nthreads + 1;
+        }
+        chunkSize = size/chunks;
+    }
+
+    for (size_t chunk = 0; chunk < chunks; chunk++){
+        for (size_t i = chunk*chunkSize; i < (size - 1 - (chunkSize * (chunks - chunk - 1))); i++){
+            threadObj.emplace_back(std::thread(fnShift, auxArray, array, i, i));    // copying back
+        }
+
+        for (std::thread& t : threadObj) {
+                if (t.joinable()) {
+                    t.join();
+                }
+        }
+    }
+
+    array[size - 1] = lastItem;
+
+    free(auxArray);
+
+}
+
 
 void blellochScan(uint16_t* array, size_t size){
     reduce(array, size);
+    uint16_t lastItem = array[size - 1];
     downSweep(array, size);
+    leftShift(array, size, lastItem);
 }
 
 
