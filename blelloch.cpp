e7aec76c021e704aaa9ac82c3bbf599b800d31e7
kchristin22 <christinakoutsou22@gmail.com>
2023-10-08 19:36:56 +0300

    Add equate function to blelloch, fix chunk number, add comments and format

--- blelloch.cpp
+++ blelloch.cpp
@@ -12,89 +12,102 @@
 
 #define SIZE 8
 
-void fnSum(uint16_t* array, size_t start, size_t end){
+void fnSum(uint16_t *array, size_t start, size_t end)
+{
     array[end] += array[start];
 }
 
-void fnSweep(uint16_t* array, size_t left, size_t right){
+void fnSweep(uint16_t *array, size_t left, size_t right)
+{
     uint16_t temp = array[left];
     array[left] = array[right];
     array[right] += temp;
 }
 
-void fnShift(uint16_t* array, uint16_t* auxArray, size_t left, size_t right){
+void fnShift(uint16_t *array, uint16_t *auxArray, size_t left, size_t right)
+{
     auxArray[left] = array[right];
 }
 
-void fnEquate(uint16_t* array, uint16_t* auxArray, size_t pos){
+void fnEquate(uint16_t *array, uint16_t *auxArray, size_t pos)
+{
     array[pos] = auxArray[pos];
 }
 
-
-void reduce(uint16_t* array, size_t size){
+void reduce(uint16_t *array, size_t size)
+{
     uint8_t nthreads = std::thread::hardware_concurrency();
     std::vector<std::thread> threadObj;
-    threadObj.reserve(nthreads);
+    threadObj.reserve(nthreads); // reserve space for the threads equal to the no. of hw threads
     size_t threadsToCreate, chunks = 1, chunkSize = size;
 
-    for(size_t k=0; k < (size_t)log2(size); k++){
+    for (size_t k = 0; k < (size_t)log2(size); k++)
+    {
         threadsToCreate = size / ((size_t)pow(2, k + 1));
 
-        if(threadsToCreate >= nthreads){
-            if (threadsToCreate % nthreads == 0){
-                chunks = threadsToCreate / nthreads;
+        if (threadsToCreate >= nthreads)
+        { // check if the number of threads needed surpasses the allocated no. of threads
+            size_t chunks = threadsToCreate / nthreads;
+            if ((threadsToCreate - (chunks * nthreads)) != 0)
+            {                // check if the number of threads needed is not a multiple of the no. of hw threads
+                chunks += 1; // add an extra chunk to account for the remaining threads
             }
-            else{
-                chunks = threadsToCreate / nthreads + 1;
-            }
-            chunkSize = size/chunks;
+            chunkSize = size / chunks;
         }
-        for (size_t threadGroup=0; threadGroup < chunks; threadGroup++){
-            for (size_t i = ((1 << k) -1 + threadGroup*chunkSize); i < (size - (1 << k) + 1 - (chunkSize*(chunks-threadGroup-1))); i+= (1 << (k+1)))
+        for (size_t threadGroup = 0; threadGroup < chunks; threadGroup++)
+        {
+            for (size_t i = ((1 << k) - 1 + threadGroup * chunkSize); i < (size - (1 << k) + 1 - (chunkSize * (chunks - threadGroup - 1))); i += (1 << (k + 1)))
+            // subtracting (chunkSize*(chunks-threadGroup-1)) to account for the chunk's limit at each iteration
+            // subtracting ((1 << k) - 1) to check if the end index is not out of bounds
             {
-                threadObj.emplace_back(std::thread(fnSum, array,i,(i + (1 << k))));
+                threadObj.emplace_back(std::thread(fnSum, array, i, (i + (1 << k))));
             }
-            for (std::thread& t : threadObj) {
-                if (t.joinable()) {
+            for (std::thread &t : threadObj)
+            { // join the parent thread before switching to the next chunk or to next level of the tree
+                if (t.joinable())
+                {
                     t.join();
                 }
             }
-            
         }
-
-        
     }
 }
 
-void downSweep(uint16_t* array, size_t size){
-    array[(1 << (int)log2(size)) - 1] = 0;
+void downSweep(uint16_t *array, size_t size)
+{
+    array[(1 << (int)log2(size)) - 1] = 0; // initiate the pivot element of the sweep
     uint8_t nthreads = std::thread::hardware_concurrency();
     std::vector<std::thread> threadObj;
-    threadObj.reserve(nthreads);
+    threadObj.reserve(nthreads); // reserve space for the threads equal to the no. of hw threads
     size_t threadsToCreate, chunks, chunkSize;
 
-    for (int k = ((int)log2(size) - 1); k >= 0; k--){
+    for (int k = ((int)log2(size) - 1); k >= 0; k--)
+    {
         threadsToCreate = size / ((size_t)pow(2, k + 1));
         chunks = 1;
         chunkSize = size;
 
-        if(threadsToCreate >= nthreads){
-            if (threadsToCreate % nthreads == 0){
-                chunks = threadsToCreate / nthreads;
+        if (threadsToCreate >= nthreads)
+        { // check if the number of threads needed surpasses the allocated no. of threads
+            size_t chunks = threadsToCreate / nthreads;
+            if ((threadsToCreate - (chunks * nthreads)) != 0)
+            {                // check if the number of threads needed is not a multiple of the no. of hw threads
+                chunks += 1; // add an extra chunk to account for the remaining threads
             }
-            else{
-                chunks = threadsToCreate / nthreads + 1;
-            }
-            chunkSize = size/chunks;
+            chunkSize = size / chunks;
         }
 
-        for (size_t chunk = 0; chunk < chunks; chunk++){
-            for (size_t i= ((1 << k) -1 + chunk*chunkSize); i <= (size - (1 << k) - (chunkSize*(chunks-chunk-1))); i+= (1 << (k+1))){ // subtracting (chunkSize*(chunks-chunk-1)) to account for the chunk's limit
-                threadObj.emplace_back(std::thread(fnSweep, array, i, (i + (1 << k))));
+        for (size_t chunk = 0; chunk < chunks; chunk++)
+        {
+            for (size_t i = ((1 << k) - 1 + chunk * chunkSize); i < (size - (1 << k) + 1 - (chunkSize * (chunks - chunk - 1))); i += (1 << (k + 1)))
+            {                                                                           // subtracting (chunkSize*(chunks-chunk-1)) to account for the chunk's limit
+                threadObj.emplace_back(std::thread(fnSweep, array, i, (i + (1 << k)))); // i + (1 << k) = 2^k  - 1 + 2^k = 2^(k+1) - 1
             }
 
-            for (std::thread& t : threadObj) {
-                if (t.joinable()) {
+            for (std::thread &t : threadObj)
+            { // join the parent thread before switching to the next chunk or to next level of the tree
+                if (t.joinable())
+                {
                     t.join();
                 }
             }
@@ -102,98 +115,112 @@ void downSweep(uint16_t* array, size_t size){
     }
 }
 
-void leftShift(uint16_t* array, size_t size, uint16_t lastItem){
+void leftShift(uint16_t *array, size_t size, uint16_t lastItem)
+{
     uint8_t nthreads = std::thread::hardware_concurrency();
-    uint8_t threadsToCreate = size / 2;
+    uint8_t threadsToCreate = size / 2; // the number of threads required is constant for all levels of the tree
     std::vector<std::thread> threadObj;
     threadObj.reserve(nthreads);
     size_t chunks = 1, chunkSize = size;
 
-    uint16_t* auxArray = (uint16_t*)malloc(SIZE*sizeof(uint16_t));
+    uint16_t *auxArray = (uint16_t *)malloc(size * sizeof(uint16_t)); // auxiliary array to be used to store the shifted array temporarily
+                                                                      // so the shifting can be done in parallel
 
-    if(threadsToCreate >= nthreads){
-        if (threadsToCreate % nthreads == 0){
-            chunks = threadsToCreate / nthreads;
+    if (threadsToCreate >= nthreads)
+    { // check if the number of threads needed surpasses the allocated no. of threads
+        size_t chunks = threadsToCreate / nthreads;
+        if ((threadsToCreate - (chunks * nthreads)) != 0)
+        {                // check if the number of threads needed is not a multiple of the no. of hw threads
+            chunks += 1; // add an extra chunk to account for the remaining threads
         }
-        else{
-            chunks = threadsToCreate / nthreads + 1;
-        }
-        chunkSize = size/chunks;
+        chunkSize = size / chunks;
     }
 
-    for (size_t chunk = 0; chunk < chunks; chunk++){
-        for (size_t i = chunk*chunkSize; i<(size - 1 - (chunkSize * (chunks - chunk - 1))); i+=2){
-            threadObj.emplace_back(std::thread(fnShift, array, auxArray, i, i + 1));    // even items
+    for (size_t chunk = 0; chunk < chunks; chunk++)
+    {
+        for (size_t i = chunk * chunkSize; i < (size - 1 - (chunkSize * (chunks - chunk - 1))); i += 2)
+        {                                                                              // subtracting 1 from size since the increment is by 2
+            threadObj.emplace_back(std::thread(fnShift, array, auxArray, i, (i + 1))); // even items
         }
 
-        for (std::thread& t : threadObj) {
-                if (t.joinable()) {
-                    t.join();
-                }
+        for (std::thread &t : threadObj)
+        {
+            if (t.joinable())
+            {
+                t.join();
+            }
         }
 
-        for (size_t i = 1 + chunk*chunkSize; i<(size-1); i+=2){
-            threadObj.emplace_back(std::thread(fnShift, array, auxArray, i, i + 1));    // odd items
+        for (size_t i = 1 + chunk * chunkSize; i < (size - 1 - (chunkSize * (chunks - chunk - 1))); i += 2)
+        {
+            threadObj.emplace_back(std::thread(fnShift, array, auxArray, i, (i + 1))); // odd items
         }
 
-        for (std::thread& t : threadObj) {
-                if (t.joinable()) {
-                    t.join();
-                }
+        for (std::thread &t : threadObj)
+        {
+            if (t.joinable())
+            {
+                t.join();
+            }
         }
     }
 
-    threadsToCreate = size;
+    // copying auxiliar array back to the original array
+    threadsToCreate = size; // each item will be copied by a single thread
     chunks = 1;
     chunkSize = size;
 
-    if(threadsToCreate >= nthreads){
-        if (threadsToCreate % nthreads == 0){
-            chunks = threadsToCreate / nthreads;
+    if (threadsToCreate >= nthreads)
+    { // check if the number of threads needed surpasses the allocated no. of threads
+        size_t chunks = threadsToCreate / nthreads;
+        if ((threadsToCreate - (chunks * nthreads)) != 0)
+        {                // check if the number of threads needed is not a multiple of the no. of hw threads
+            chunks += 1; // add an extra chunk to account for the remaining threads
         }
-        else{
-            chunks = threadsToCreate / nthreads + 1;
-        }
-        chunkSize = size/chunks;
+        chunkSize = size / chunks;
     }
 
-    for (size_t chunk = 0; chunk < chunks; chunk++){
-        for (size_t i = chunk*chunkSize; i < (size - 1 - (chunkSize * (chunks - chunk - 1))); i++){
-            threadObj.emplace_back(std::thread(fnShift, auxArray, array, i, i));    // copying back
+    for (size_t chunk = 0; chunk < chunks; chunk++)
+    {
+        for (size_t i = chunk * chunkSize; i < (size - 1 - (chunkSize * (chunks - chunk - 1))); i++)
+        {
+            threadObj.emplace_back(std::thread(fnEquate, array, auxArray, i));
         }
 
-        for (std::thread& t : threadObj) {
-                if (t.joinable()) {
-                    t.join();
-                }
+        for (std::thread &t : threadObj)
+        {
+            if (t.joinable())
+            {
+                t.join();
+            }
         }
     }
 
-    array[size - 1] = lastItem;
+    array[size - 1] = lastItem; // the last item that was overwritten by the pivot element of the sweep
 
-    free(auxArray);
-
+    free(auxArray); // deallocate the memory used by the auxiliary array
 }
 
-
-void blellochScan(uint16_t* array, size_t size){
-    reduce(array, size);
-    uint16_t lastItem = array[size - 1];
-    downSweep(array, size);
-    leftShift(array, size, lastItem);
+void blellochScan(uint16_t *array, size_t size)
+{
+    reduce(array, size);                 // first stage of the algorithm
+    uint16_t lastItem = array[size - 1]; // save the calculated value of the last item to re-assign it later
+    downSweep(array, size);              // second stage of the algorithm
+    leftShift(array, size, lastItem);    // use left shift to convert result to inclusive scan's output
 }
-
 
 // check for overflows
 
-int main(){
+int main()
+{
 
-    uint16_t* array = (uint16_t*)malloc(SIZE*sizeof(uint16_t));
+    uint16_t *array = (uint16_t *)malloc(SIZE * sizeof(uint16_t));
 
     // srand(time(NULL));
 
     // printf("The array in question is: ");
-    for (size_t i=0; i<SIZE; i++){
+    for (size_t i = 0; i < SIZE; i++)
+    {
         array[i] = rand() % 100;
         // printf("%d ", array[i]);
     }
@@ -213,17 +240,14 @@ int main(){
     std::fstream file("output_blelloch.json", std::ios::out);
 
     ankerl::nanobench::Bench()
-    .minEpochIterations(100)
-    .epochs(30) 
-    .run("blelloch", [&] {
-        blellochScan(array, SIZE);
-    })
-    .render(
-        ankerl::nanobench::templates::pyperf(),
-        file
-    );
+        .minEpochIterations(100)
+        .epochs(30)
+        .run("blelloch", [&]
+             { blellochScan(array, SIZE); })
+        .render(
+            ankerl::nanobench::templates::pyperf(),
+            file);
 
     free(array);
     return 0;
 }
-
--- genInclusiveScan.cpp
+++ genInclusiveScan.cpp
@@ -8,137 +8,134 @@
 
 #define SIZE 8
 
-void fnSum(uint16_t* array, size_t start, size_t end){
+void fnSum(uint16_t *array, size_t start, size_t end)
+{
     array[end] += array[start];
 }
 
-void fnTree(uint16_t* array, size_t size, size_t start){
+void fnTree(uint16_t *array, size_t start)
+{
 
     uint8_t nthreads = std::thread::hardware_concurrency();
     std::vector<std::thread> threadObj;
     threadObj.reserve(nthreads);
-    size_t threadsToCreate, chunks = 1, chunkSize = start;
-    size_t current_pos, previous_pos, end;
+    size_t threadsToCreate, chunks = 1, chunkSize = start; // the length of the array range we're dealing with here is equal to start(2^i)
+    size_t current_pos, previous_pos;                      // previous_pos is the position of the nearest item from the left
+                                                           // that includes the sum of all its previous items and itself
 
-    size_t pow2size = 1 << (size_t)log2(size);
-
-    if (size % 2 != 0){
-        size--;
-    }
-
-    if (size - pow2size > 0 && start == pow2size && (size - start) % 2 != 0){     // size != 2^k and index not in an odd position
-        end = 2 + size;
-    }
-    else end = 2 * start;
-
-    for(size_t l= 1; l < ((size_t)log2(start) + 1); l++){
+    for (size_t l = 1; l < ((size_t)log2(start) + 1); l++)
+    {
         threadsToCreate = (size_t)pow(2, l - 1);
 
-        if(threadsToCreate >= nthreads){
-            if (threadsToCreate % nthreads == 0){
-                chunks = threadsToCreate / nthreads;
+        if (threadsToCreate >= nthreads)
+        { // check if the number of threads needed surpasses the allocated no. of threads
+            size_t chunks = threadsToCreate / nthreads;
+            if ((threadsToCreate - (chunks * nthreads)) != 0)
+            {                // check if the number of threads needed is not a multiple of the no. of hw threads
+                chunks += 1; // add an extra chunk to account for the remaining threads
             }
-            else{
-                chunks = threadsToCreate / nthreads + 1;
-            }
-            chunkSize = start/chunks;
+            chunkSize = start / chunks;
         }
-        for (size_t chunk=0; chunk < chunks; chunk++){
-            for (size_t k = 1 + chunk*chunkSize; k < ((1 << l) - (chunkSize*(chunks-chunk-1))); k+=2)
+        for (size_t chunk = 0; chunk < chunks; chunk++)
+        {
+            for (size_t k = 1 + chunk * chunkSize; k < ((1 << l) - (chunkSize * (chunks - chunk - 1))); k += 2) // check report for an explanation on the limit of k
             {
-                current_pos = start - 1 + k * (end - start) / (1 << l);
-                previous_pos = start - 1 + (k - 1) * (end - start) / (1 << l);
-                threadObj.emplace_back(std::thread(fnSum, array,previous_pos,current_pos));
+                current_pos = start - 1 + k * (1 << ((size_t)log2(start) - l)); // check report for an explanation on the formula
+                previous_pos = start - 1 + (k - 1) * (1 << ((size_t)log2(start) - l));
+                threadObj.emplace_back(std::thread(fnSum, array, previous_pos, current_pos));
             }
-            for (std::thread& t : threadObj) {
-                if (t.joinable()) {
+            for (std::thread &t : threadObj)
+            {
+                if (t.joinable())
+                {
                     t.join();
                 }
             }
-            
-        }   
+        }
     }
-
-    if (size % 2 != 0){
-        array[size-1] += array[size - 2];
-    }
-
 }
 
-
-void reduce(uint16_t* array, size_t size){
+void reduce(uint16_t *array, size_t size)
+{
     uint8_t nthreads = std::thread::hardware_concurrency();
     std::vector<std::thread> threadObj;
-    threadObj.reserve(nthreads);
+    threadObj.reserve(nthreads); // reserve space for the threads equal to the no. of hw threads
     size_t threadsToCreate, chunks = 1, chunkSize = size;
 
-    for(size_t k=0; k < (size_t)log2(size); k++){
+    for (size_t k = 0; k < (size_t)log2(size); k++)
+    {
         threadsToCreate = size / ((size_t)pow(2, k + 1));
 
-        if(threadsToCreate >= nthreads){
-            if (threadsToCreate % nthreads == 0){
-                chunks = threadsToCreate / nthreads;
+        if (threadsToCreate >= nthreads)
+        { // check if the number of threads needed surpasses the allocated no. of threads
+            size_t chunks = threadsToCreate / nthreads;
+            if ((threadsToCreate - (chunks * nthreads)) != 0)
+            {                // check if the number of threads needed is not a multiple of the no. of hw threads
+                chunks += 1; // add an extra chunk to account for the remaining threads
             }
-            else{
-                chunks = threadsToCreate / nthreads + 1;
-            }
-            chunkSize = size/chunks;
+            chunkSize = size / chunks;
         }
-        for (size_t threadGroup=0; threadGroup < chunks; threadGroup++){
-            for (size_t i = ((1 << k) -1 + threadGroup*chunkSize); i < (size - (1 << k) + 1 - (chunkSize*(chunks-threadGroup-1))); i+= (1 << (k+1)))
+        for (size_t threadGroup = 0; threadGroup < chunks; threadGroup++)
+        {
+            for (size_t i = ((1 << k) - 1 + threadGroup * chunkSize); i < (size - (1 << k) + 1 - (chunkSize * (chunks - threadGroup - 1))); i += (1 << (k + 1)))
+            // subtracting (chunkSize*(chunks-threadGroup-1)) to account for the chunk's limit at each iteration
+            // subtracting ((1 << k) - 1) to check if the end index is not out of bounds
             {
-                threadObj.emplace_back(std::thread(fnSum, array,i,(i + (1 << k))));
+                threadObj.emplace_back(std::thread(fnSum, array, i, (i + (1 << k))));
             }
-            for (std::thread& t : threadObj) {
-                if (t.joinable()) {
+            for (std::thread &t : threadObj)
+            { // join the parent thread before switching to the next chunk or to next level of the tree
+                if (t.joinable())
+                {
                     t.join();
                 }
             }
-            
         }
-
-        
     }
 }
 
-void increase(uint16_t* array, size_t size){
+void increase(uint16_t *array, size_t size)
+{
     uint8_t nthreads = std::thread::hardware_concurrency();
     std::vector<std::thread> threadObj;
     threadObj.reserve(nthreads);
 
     size_t log2size = (size_t)log2(size);
 
-    size_t limit = (size - (1 << log2size)) > 0 ? log2size + 1 : log2size;
+    size_t limit = (size - (1 << log2size)) > 0 ? log2size + 1 : log2size; // check if the size of the array is not a power of 2
 
-    for (size_t i = 1; i < limit; i++){
-        threadObj.emplace_back(std::thread(fnTree, array, size, (1 << i)));
+    for (size_t i = 1; i < limit; i++)
+    { // divide the array to ranges of [2^i, 2^(i+1))
+        threadObj.emplace_back(std::thread(fnTree, array, (1 << i)));
     }
 
-    for (std::thread& t : threadObj) {
-        if (t.joinable()) {
+    for (std::thread &t : threadObj)
+    { // join the parent thread before exiting
+        if (t.joinable())
+        {
             t.join();
         }
     }
- 
 }
 
-
-void scan(uint16_t* array, size_t size){
-    reduce(array, size);
-    increase(array, size);
+void scan(uint16_t *array, size_t size)
+{
+    reduce(array, size);   // first stage of the algorithm, same with blelloch's
+    increase(array, size); // second stage of the algorithm
 }
-
 
 // check for overflows
 
-int main(){
+int main()
+{
 
-    uint16_t* array = (uint16_t*)malloc(SIZE*sizeof(uint16_t));
+    uint16_t *array = (uint16_t *)malloc(SIZE * sizeof(uint16_t));
 
     // srand(time(NULL));
 
     printf("The array in question is: ");
-    for (size_t i=0; i<SIZE; i++){
+    for (size_t i = 0; i < SIZE; i++)
+    {
         array[i] = rand() % 100;
         printf("%d ", array[i]);
     }
@@ -150,7 +147,8 @@ int main(){
     printf("Time elapsed: %ld\n", duration.count());
 
     printf("Final:");
-    for (size_t i=0; i<SIZE; i++){
+    for (size_t i = 0; i < SIZE; i++)
+    {
         printf(" %d", array[i]);
     }
     printf("\n");
